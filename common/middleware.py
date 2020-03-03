fb62aca0d47d278c165ed1a42575a7c4758de488
Jazzep <dble_s@outlook.com>
2020-03-03 16:33:54 +0800

    user profile got

--- common/middleware.py
+++ common/middleware.py
@@ -5,7 +5,18 @@ from common import error
 
 class AuthMiddleware(MiddlewareMixin):
 	'''用户登录验证中间件'''
+
+	WHITE_LIST = [
+		'api/user/verify',
+		'api/user/login',
+	]
+
 	def process_request(self, request):
+		# 如果请求的 URL 在白名单内，直接跳过检查
+		for path in self.WHITE_LIST:
+			if request.path in self.WHITE_LIST:
+				return
+		# 进行登录检查
 		uid = request.session.get('uid')
 		if uid:
 			try:
